41aced7f217b9dfbdfb51ad061267d4a599dc71b
fkbinho <sir_kleber@hotmail.com>
2025-03-25 18:37:28 -0300

    move dto package to controller package

--- src/main/java/com/fkbinho/product_api/controller/dto/ProductDTO.java
+++ src/main/java/com/fkbinho/product_api/controller/dto/ProductDTO.java
@@ -1,4 +1,4 @@
-package com.fkbinho.product_api.dto;
+package com.fkbinho.product_api.controller.dto;
 
 import com.fkbinho.product_api.domain.model.Product;
 
@@ -30,31 +30,15 @@ public class ProductDTO {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Double getPrice() {
         return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
     }
 }
